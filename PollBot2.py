571da4513bffc3d8180996eae0149de07066b05a
Liam Pang-Naylor <61168583+liamjet@users.noreply.github.com>
2022-03-19 12:35:01 -0700

    Updated poll functionality
    Added numbers before each option.

--- PollBot2.py
+++ PollBot2.py
@@ -11,18 +11,14 @@ bot = commands.Bot(command_prefix='jbot ')
 emojis = ['<:jbotone:935711058457423882>','<:jbottwo:935711121812361246>','<:jbotthree:935711157380079657>','<:jbotfour:935711194491256832>','<:jbotfive:935711229912170566>','<:jbotsix:935711265731514459>','<:jbotseven:935711297801191474>','<:jboteight:935711328289562714>','<:jbotnine:935711357758763059>']
 
 @bot.command(brief='Creates a poll with up to 9 options, place a space between each option.', description='To create a poll, type the following format: "jbot create option1 option2" where the options are the desired voting sections.')
-async def create(ctx,*args):
-    poll_options = list(args)
-    #poll_options = [None] * (len(args)-1)
-    #for x in args:
-        #i = 0
-        #poll_options[i] = str(i+1) + ': ' + args[i] 
-    #poll_options = tuple(poll_options)
-        
-        
-    message = await ctx.send('{} options: {}'.format(len(args), ', '.join(poll_options)))
-    for x in range(len(args)):
-        await message.add_reaction(emojis[x])
+async def poll(ctx,*args):      
+    args_list = list(args)
+    for x in range(len(args_list)):
+        args_list[x] = '{}. {}'.format(x+1, args_list[x])
+    args = tuple(args_list)
+    message = await ctx.send('{} voting options:\n{}'.format(len(args), '\n'.join(args)))
+    for y in range(len(args)):
+        await message.add_reaction(emojis[y])
 
 @bot.command(brief='Rolls a dice of your choosing.', description='To roll a dice, type the following format: "jbot roll #" where # is any number.')
 async def roll(ctx,*args):
